7817aaa26ff4c842c89d6c1dcb5fc12627ff9671
Dawson Kroeker <dkroeker@gmail.com>
2014-12-16 08:43:01 -0600

    learnyounode: http json api server

--- learnyounode/13_http_json_api_server.js
+++ learnyounode/13_http_json_api_server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var url = require('url');
+var moment = require('moment');
 
 var port = Number(process.argv[2]);
 
@@ -7,12 +8,36 @@ var server = http.createServer(requestListener);
 server.listen(port);
 console.log('Listening on port ' + port);            
 
+var urlMatcher = /\/api\/([^\/?\s]+)/;
 function requestListener(request, response) {
-    console.log('Received ' + request.method + ' request');
-    if (request.method != 'GET') {        
-        response.statusCode = 501;
-        return response.end(request.method + ' not supported');
+    var urlDetails = url.parse(request.url, true);
+    var action = urlMatcher.exec(urlDetails.pathname)[1];
+    
+    var timeResponse = getTimeResult(action, urlDetails.query);
+    if (timeResponse) {
+        response.writeHead(200, {'Content-Type': 'application/json' });
+        response.end(JSON.stringify(timeResponse));
     }
-    
-    
+    else {
+        response.writeHead(404);
+        response.end();
+    }    
+}
+
+function getTimeResult(action, query) {
+    var json = {};
+    var time = moment(query.iso);
+    if (action === 'parsetime') {
+        return {
+            hour: time.hour(),
+            minute: time.minute(),
+            second: time.second()
+        };
+    }
+    else if (action === 'unixtime') {
+        return {
+            unixtime: time.valueOf()
+        }        
+    }
+    return null;
 }
