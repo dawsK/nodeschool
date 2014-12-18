23f7f61383836b0ba6556f2b651adcb56aa58b12
Dawson Kroeker <dkroeker@gmail.com>
2014-12-17 21:53:58 -0600

    stream-adventure: Lines

---
+++ stream-adventure/5_lines.js
@@ -0,0 +1,18 @@
+var split = require('split');
+var through = require('through');
+
+var lineNum = 1;
+var caseStream = through(function (line) {
+    if (lineNum % 2 == 0) {
+        this.queue(line.toString().toUpperCase() + '\n');
+    }
+    else {
+        this.queue(line.toString().toLowerCase() + '\n');
+    }
+    lineNum += 1;
+});
+
+process.stdin
+    .pipe(split())
+    .pipe(caseStream)
+    .pipe(process.stdout);
